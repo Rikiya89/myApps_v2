5f34628dbe1fc0ce9a22f023c945e575c2b489ca
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2024-06-04 01:09:34 +0900

    20240604

--- 20240604/src/ofApp.cpp
+++ 20240604/src/ofApp.cpp
@@ -1,81 +1,44 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+    image.load("eye.jpg");
 
+    mesh.setMode(OF_PRIMITIVE_POINTS);
+
+    for (int y = 0; y < image.getHeight(); y++) {
+        for (int x = 0; x < image.getWidth(); x++) {
+            ofColor color = image.getColor(x, y);
+            float brightness = color.getBrightness();
+            float z = ofMap(brightness, 0, 255, -100, 100); // Map brightness to depth
+
+            ofVec3f position(x - image.getWidth() / 2, y - image.getHeight() / 2, z);
+            mesh.addVertex(position);
+            mesh.addColor(color);
+            originalVertices.push_back(position);
+        }
+    }
+
+    ofEnableDepthTest();
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::update() {
+    float time = ofGetElapsedTimef();
+    for (int i = 0; i < originalVertices.size(); ++i) {
+        ofVec3f vertex = originalVertices[i];
+        float wave = sin(vertex.x * 0.05 + time) * 20.0;
+        vertex.z += wave;
+        mesh.setVertex(i, vertex);
+    }
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+    ofBackground(0);
+    ofPushMatrix();
+    ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+    ofRotateYDeg(ofGetElapsedTimef() * 10); // Rotate the point cloud
 
-}
+    glPointSize(2); // Set point size
+    mesh.draw();
 
-//--------------------------------------------------------------
-void ofApp::exit(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    ofPopMatrix();
 }
