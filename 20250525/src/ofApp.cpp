6f8343c1bfd859d9ebbd6699cea53bff44ae5f87
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2025-05-26 01:28:38 +0900

    20250526

--- 20250525/src/ofApp.cpp
+++ 20250525/src/ofApp.cpp
@@ -2,17 +2,73 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofBackground(0);
+    fbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    fbo.begin();
+    ofClear(0, 0, 0, 255);
+    fbo.end();
 
+    int numPoints = 500;
+    for (int i = 0; i < numPoints; i++) {
+        float angle = ofMap(i, 0, numPoints, 0, TWO_PI);
+        float radius = 200;
+        float x = ofGetWidth() / 2 + radius * cos(angle);
+        float y = ofGetHeight() / 2 + radius * sin(angle);
+        points.push_back(ofPoint(x, y));
+    }
+    noiseOffset = 0.0;
+    time = 0.0;
+    hue = 0.0;
+    symmetry = 6; // sacred geometry inspired
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    noiseOffset += 0.01;
+    time += 0.01;
+    hue += 0.5;
+    if (hue > 255) hue -= 255;
+    for (auto& p : points) {
+        float noise = ofNoise(p.x * 0.005, p.y * 0.005, noiseOffset);
+        float angle = noise * TWO_PI * 2;
+        p.x += cos(angle) * 0.5;
+        p.y += sin(angle) * 0.5;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    fbo.begin();
+    ofEnableAlphaBlending();
+    ofSetColor(0, 0, 0, 10); // fade effect
+    ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
 
+    ofPushMatrix();
+    ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+    for (int i = 0; i < symmetry; i++) {
+        ofPushMatrix();
+        ofRotateDeg(360.0 / symmetry * i);
+
+        ofColor c = ofColor::fromHsb(static_cast<int>(hue), 200, 255);
+        ofSetColor(c);
+        ofNoFill();
+        ofBeginShape();
+        for (auto& p : points) {
+            ofVertex(p.x - ofGetWidth() / 2, p.y - ofGetHeight() / 2);
+        }
+        ofEndShape(true);
+
+        for (auto& p : points) {
+            ofDrawCircle(p.x - ofGetWidth() / 2, p.y - ofGetHeight() / 2, 1.5);
+        }
+
+        ofPopMatrix();
+    }
+    ofPopMatrix();
+
+    fbo.end();
+
+    fbo.draw(0, 0);
 }
 
 //--------------------------------------------------------------
